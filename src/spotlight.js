256f3f6dfcfa7424a35c6a02376d9b3426ca4054
Jesse Millar <hellojessemillar@gmail.com>
2015-02-05 09:38:52 -0700

    Better light positioning

--- src/spotlight.js
+++ src/spotlight.js
@@ -1,16 +1,17 @@
 var SpotLight = function()
 {
+	this.position = new Object()
+
 	this.shadows = new Object()
 		this.shadows.cast = false
-		this.shadows.darkness = 0.5
 
 	this.three = new Object() // Keep all the three.js stuff here
 
 	this.setPosition = function(x, y, z)
 	{
-		this.x = x
-		this.y = y
-		this.z = z
+		this.position.x = x
+		this.position.y = y
+		this.position.z = z
 
 		return this
 	}
@@ -31,25 +32,28 @@ var SpotLight = function()
 
 	this.add = function()
 	{
-		this.three.light = new THREE.SpotLight('#ffffff')
-		this.three.light.position.x = this.x
-		this.three.light.position.y = this.y
-		this.three.light.position.z = this.z
+		this.three.light = new THREE.SpotLight(0xffffff)
 		this.three.light.intensity = this.intensity
 
 		if (this.shadows.cast)
 		{
 			console.log('Light shadow casting enabled')
 			this.three.light.castShadow = true
-			this.three.light.shadowDarkness = this.shadows.darkness
 			this.three.light.shadowCameraVisible = true
 
-			this.three.light.shadowCameraRight    =  5;
-			this.three.light.shadowCameraLeft     = -5;
-			this.three.light.shadowCameraTop      =  5;
-			this.three.light.shadowCameraBottom   = -5;
+			this.three.light.shadowMapWidth = 1024
+			this.three.light.shadowMapHeight = 1024
 		}
+
+		this.apply()
 
 		f.scene.add(this.three.light)
 	}
+
+		this.apply = function()
+		{
+			this.three.light.position.x = this.position.x
+			this.three.light.position.y = this.position.y
+			this.three.light.position.z = this.position.z
+		}
 }
